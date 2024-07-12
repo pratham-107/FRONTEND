7b7fec2604e7705db0cd0b1c354ab3b662baa4d8
pratham-107 <spratham845@gmail.com>
2024-07-12 17:30:56 +0530

    code added

---
+++ JAVASCRIPT/lecture 1/new.js
@@ -0,0 +1,17 @@
+var a='5'
+let b ='helllo'
+const c =7
+
+console .log(a);
+console.log(b);
+console.log(c);
+
+let firstName='pratham'
+let lastName='singh'
+
+console.log(`${firstName}            ${lastName}`);
+// console.log(firstName+"         "+lastName);
+
+
+// let a=false
+// console.log(a);
