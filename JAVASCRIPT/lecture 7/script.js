e558969ec64be31d2a645c2b68cdce92fb07f775
pratham-107 <spratham845@gmail.com>
2024-08-03 11:53:20 +0530

    code updated

--- JAVASCRIPT/lecture 7/script.js
+++ JAVASCRIPT/lecture 7/script.js
@@ -1,30 +1,30 @@
-function step1(fn,heee){
-    setTimeout(() => {
-        console.log('photo selected');
-        fn()
-    }, 5000);
-}
+// function step1(fn,heee){
+//     setTimeout(() => {
+//         console.log('photo selected');
+//         fn()
+//     }, 5000);
+// }
 
-function step2(fn){
-    setTimeout(() => {
-        console.log('filteer');
-        fn()
-    }, 4000);
-}
+// function step2(fn){
+//     setTimeout(() => {
+//         console.log('filteer');
+//         fn()
+//     }, 4000);
+// }
 
-function step3(fn){
-    setTimeout(() => {
-        console.log('captions added');
-        fn()
-    }, 3000);
-}
+// function step3(fn){
+//     setTimeout(() => {
+//         console.log('captions added');
+//         fn()
+//     }, 3000);
+// }
 
-function step4(fn){
-    setTimeout(() => {
-        console.log('photo posted');
-        fn()
-    }, 2000);
-}
+// function step4(fn){
+//     setTimeout(() => {
+//         console.log('photo posted');
+//         fn()
+//     }, 2000);
+// }
 
 // step1()
 // step2()
@@ -33,40 +33,40 @@ function step4(fn){
 
 
 //CALLBACK HELL//
-step1(()=>{
-    step2(()=>{
-        step3(()=>{
-            step4()
-        })
-    })
-})
+// step1(()=>{
+//     step2(()=>{
+//         step3(()=>{
+//             step4()
+//         })
+//     })
+// })
 
 
 
 //PROMISES//
- let promise = new Promise((resolve, reject) => {
+//  let promise = new Promise((resolve, reject) => {
 
 
-    let reason ='barish'
-    if (reason == 'barish'){
-        reject('err')
-    }
-    else{
-        resolve('resolve')
-    }
+//     let reason ='barish'
+//     if (reason == 'barish'){
+//         reject('err')
+//     }
+//     else{
+//         resolve('resolve')
+//     }
 
-    // setTimeout(() => {
-        // resolve()
-        // reject()
- })
+//     // setTimeout(() => {
+//         // resolve()
+//         // reject()
+//  })
 
- console.log(promise,"hello world");
+//  console.log(promise,"hello world");
 
- promise.then((a)=>{
-    console.log(a);
- }).catch((err)=>{
-    console.log(err);
- })
+//  promise.then((a)=>{
+//     console.log(a);
+//  }).catch((err)=>{
+//     console.log(err);
+//  })
 
 
 
@@ -85,7 +85,7 @@ function step1() {
 function step2() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log('filter');
+            console.log('filter applied');
             resolve();
         }, 4000);
     });
@@ -94,7 +94,7 @@ function step2() {
 function step3() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log('captions added');
+            console.log('caption added');
             resolve();
         }, 3000);
     });
@@ -109,17 +109,25 @@ function step4() {
     });
 }
 
-step1()
-    .then(() => step2())
-    .then(() => step3())
-    .then(() => step4())
-    .catch((error) => console.error('error occurred:', error));
+// step1()
+//     .then(() => step2())
+//     .then(() => step3())
+//     .then(() => step4())
+//     .catch((error) => console.error('error occurred:', error));
 
 
+//ASYNC AND AWAIT ///
 
-// async function magic(){
-//     let data= await step1()
-//     console.log(data);
-//     let data1= await step2()
-//     console.log(data1);
-// }
+async function magic(){
+    let data= await step1()
+    console.log(data);
+    let data1= await step2()
+    console.log(data1);
+    let data2= await step3()
+    console.log(data2)
+    let data3= await step4
+    console.log(data3)
+    let data4 = await step4()
+    console.log(data4)
+}
+magic()
---
+++ JAVASCRIPT/lecture 8/script.js
@@ -0,0 +1,19 @@
+// DOM //
+
+// document.getElementById('one')
+// let btn = document.getElementById('one')
+
+// document.getElementsByClassName('two')
+// let ptn = document.getElementsByClassName('two')
+
+// document.querySelector('one')
+
+// let bttn = document.getElementsByClassName('two')
+
+// let mtn = document.querySelectorAll('h1')
+
+let mtn = document.querySelectorAll('h1')
+for (let i of mtn){
+    i.style.color='red'
+}
+console.log("mtn");
