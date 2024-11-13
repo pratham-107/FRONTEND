7ba2f965bc48460f7befe15433ca6b440b5112a1
pratham-107 <spratham845@gmail.com>
2024-11-13 15:11:02 +0530

    code update

---
+++ JAVASCRIPT/lecture 10/script.js
@@ -0,0 +1,29 @@
+let input = document.querySelector('input');
+let btn = document.querySelector('button');
+let list = document.getElementById('list');
+
+function fetchData(searchText) {
+  axios.get(`https://api.tvmaze.com/search/shows?q=${searchText}`)
+  .then(function(response) {
+    manipulateDOM(response.data);
+  })
+}
+
+function manipulateDOM(data) {
+  while(list.firstChild) {
+    list.firstChild.remove();
+  }
+
+  for (let d of data) {
+    if (d.show.image) {
+      let img = document.createElement('img');
+      img.setAttribute('src', d.show.image.original);
+      list.appendChild(img);
+    }  
+  }
+}
+
+btn.addEventListener('click', function() {
+  let searchText = input.value;
+  let data = fetchData(searchText);
+})
